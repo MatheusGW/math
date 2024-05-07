95104cd426186d5f52dd2adb91b017113541ad0d
Meth <wilczek.matheus@escola.pr.gov.br>
2024-05-07 10:10:03 -0300

    criação

---
+++ nome.js
@@ -0,0 +1,147 @@
+let T = 1
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
+
+console.log("================");
+
+T++
+
+console.log(T +"x1 =", T * 1);
+console.log(T +"x2 =", T * 2);
+console.log(T +"x3 =", T * 3);
+console.log(T +"x4 =", T * 4);
+console.log(T +"x5 =", T * 5);
+console.log(T +"x6 =", T * 6);
+console.log(T +"x7 =", T * 7);
+console.log(T +"x8 =", T * 8);
+console.log(T +"x9 =", T * 9);
+console.log(T +"x10 =", T * 10);
